f326028dcd5cd046f1018142041c7150997f14cb
SaltSith66 <SaltSith66@gmail.com>
2023-01-08 16:36:19 +0100

    printing image info

--- image/image.c
+++ image/image.c
@@ -10,10 +10,26 @@ image_print_info(const image_type_t *image)
 
     printf("%d %d \r\n", image->x, image->y);
 
-    for (uint32_t i = 0; i < (image->x * image->y); i++) {
-        printf("%d ", image->pixels_backup[i]);
-        if (((i + 1) % image->x) == 0) {
-            printf("\r\n");
+    if (image->magic_number > P1) {
+        printf("%d\n", image->grey_scale);
+    }
+
+    if (image->magic_number <= P2) {
+        for (uint32_t i = 0; i < (image->x * image->y); i++) {
+            printf("%d ", image->pixels_backup[i]);
+            if (((i + 1) % image->x) == 0) {
+                printf("\r\n");
+            }
+        }
+    } else {
+        for (uint32_t i = 0; i < (3 * image->x * image->y); i++) {
+            printf("%d ", image->pixels_backup[i]);
+            if ((i + 1) % 3 == 0) {
+                printf("  ");
+            }
+            if ((i + 1) % (3 * image->x) == 0) {
+                printf("\n");
+            }
         }
     }
 }
